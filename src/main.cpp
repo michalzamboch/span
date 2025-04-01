d79fb9c62eeeb39c8d5450da9b8cc3cdd96ee437
Michal Žamboch <zamboch.michal@seznam.cz>
2025-04-01 21:30:30 +0200

    std array

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,8 @@
 #include <iostream>
 #include <span>
+#include <vector>
+#include <array>
 #include "custom_iterator.h"
-
-class Span
-{
-    size_t size;
-    type* pointer;
-}
-
-
-
-
-
 
 void printSpan(std::span<int> numbers)
 {
@@ -36,6 +27,9 @@ int main(int argc, char** argv)
 
     std::vector<int> intVector {1,2,3,4,5,6,7,8,9};
     printSpan(intVector);
+
+    std::array<int, 4> intArray {4,3,2,1};
+    printSpan(intArray);
 
     std::string message {"Hello world"};
     printString(message);
