0099e040096888265116a053853b3065e0b3f4c4
Michal Žamboch <zamboch.michal@seznam.cz>
2025-04-01 21:53:19 +0200

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,6 @@
 #include <array>
 #include "custom_iterator.h"
 
-void printSpan(std::span<int> numbers)
-{
-    if (numbers.extent == std::dynamic_extent)
-    {
-        std::cout << "dynamic" << std::endl;
-    }
-    else
-    {
-        std::cout << "static" << std::endl;
-    }
-
-    for (const auto& num : numbers) {
-        std::cout << num << " ";
-    } 
-    std::cout << std::endl;
-}
-
 void printString(std::span<char> numbers)
 {
     for (const auto& num : numbers) {
@@ -29,12 +12,43 @@ void printString(std::span<char> numbers)
     std::cout << std::endl;
 }
 
+void printSpan(std::span<int> numbers)
+{
+    for (const auto& num : numbers) {
+        std::cout << num << " ";
+    } 
+    std::cout << std::endl;
+}
+
+void setSpan(std::span<int> numbers, int value)
+{
+    for (auto& num : numbers) {
+        num = value;
+    } 
+}
+
+void setSpanValue(std::span<int> numbers, int value, int pos)
+{
+    if (numbers.size() <= pos)
+    {
+        std::cout << "Position is too high!" << std::endl;
+        return;
+    }
+
+    numbers[pos] = value;
+}
+
 int main(int argc, char** argv)
 {
     int array[5] = {1,2,3,4,5};
+    setSpanValue(array, 100, 10);
+    setSpanValue(array, 100, 1);
     printSpan(array);
 
     std::vector<int> intVector {1,2,3,4,5,6,7,8,9};
+    printSpan(intVector);
+    setSpan(intVector, 1);
+    setSpanValue(intVector, 100, 0);
     printSpan(intVector);
 
     std::array<int, 4> intArray {4,3,2,1};
